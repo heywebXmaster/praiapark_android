58e12755150438d9d232ce33dbf12ead1d12ccbd
devin <devin@devins-MacBook-Pro-2.local>
2019-11-29 10:37:59 +0800

    change key for push and log

--- app/src/main/java/com/example/building/mvp/presenter/PushTokenInitPresenter.java
+++ app/src/main/java/com/example/building/mvp/presenter/PushTokenInitPresenter.java
@@ -1,10 +1,14 @@
 package com.example.building.mvp.presenter;
 
+import android.util.Log;
+
 import com.example.building.bean.BaseEntity;
 import com.example.building.config.LocalSaveData;
 import com.example.building.mvp.contract.PushTokenInitContract;
 import com.example.building.net.BaseObserver;
 import com.example.building.net.RetrofitFactory;
+
+import cn.jpush.android.api.JPushInterface;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -12,6 +16,8 @@ public class PushTokenInitPresenter implements PushTokenInitContract.PushTokenIn
 
     @Override
     public void uploadPushToken(String regId) {
+        Log.e("PushTokenInitPresenter", "regId: " + regId);
+
         RetrofitFactory.getInstence().API().initToken(
                 LocalSaveData.getInstance().getUserName(),
                 regId,
@@ -22,7 +28,7 @@ public class PushTokenInitPresenter implements PushTokenInitContract.PushTokenIn
 
                     @Override
                     protected void onSuccees(BaseEntity<String> t) {
-
+                        Log.i("XXXXX",t.toString());
                     }
 
                     @Override
